8e371525d073e55e840a6e26d8c0ca2d60323523
dimassupandi <dimassupandi8@gmail.com>
2024-05-30 09:19:15 +0700

    Initial commit

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+    * Run the database seeds.
+    */
+    public function run(): void
+    {
+    // Creating Super Admin User
+    $superAdmin = User::create([
+        'name' => 'Rivo',
+        'email' => 'superadmin@roles.id',
+        'password' => Hash::make('123456')
+        ]);
+        $superAdmin->assignRole('Super Admin');
+    // Creating Admin User
+    $admin = User::create([
+        'name' => 'Meri',
+        'email' => 'admin@roles.id',
+        'password' => Hash::make('123456')
+        ]);
+        $admin->assignRole('Admin');
+    // Creating Product Manager User
+    $productManager = User::create([
+        'name' => 'Fahri',
+        'email' => 'operator@roles.id',
+        'password' => Hash::make('123456')
+        ]);
+        $productManager->assignRole('Operator');
+    // Creating Admin Baak User
+        $adminbaak = User::create([
+            'name' => 'Keanue Ban tubles',
+            'email' => 'adminbaak@roles.id',
+            'password' => Hash::make('123456')
+            ]);
+            $adminbaak->assignRole('Admin Baak');
+
+    // Creating Admin Keuangan User
+        $adminkeuangan = User::create([
+            'name' => 'Jacly Natsir',
+            'email' => 'adminkeuangan@roles.id',
+            'password' => Hash::make('123456')
+            ]);
+            $adminkeuangan->assignRole('Admin Keuangan');
+
+    // Creating Mahasiswa User
+        $mahasiswa = User::create([
+            'name' => 'Dhani Raynaldi',
+            'email' => 'mahasiswa@roles.id',
+            'password' => Hash::make('123456')
+            ]);
+            $mahasiswa->assignRole('Mahasiswa');
+    }
+}
